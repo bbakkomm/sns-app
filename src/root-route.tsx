53b8b9f5ad90661fb79259465cbcd748a40baffe
bbakkomm <bbakkomm@gmail.com>
2026-05-30 17:05:55 +0900

    first commit

---
+++ src/root-route.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+
+export default function RootRoute() {
+  return <Routes>
+      <Route/>
+      <Route/>
+      <Route/>
+      <Route/>
+      <Route/>
+    </Routes>
+}
